60a722f64a9a57ed6855fd06ce3297c714472b4b
BitterToGitter <jakimowichmaxim@gmail.com>
2024-02-15 19:42:50 +0200

    Nightlearncheck

--- app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
+++ app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
@@ -3,12 +3,24 @@ package jakimovich.nightlearn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etLastname;
+    EditText etNickname;
+    EditText etEmail;
+    EditText etPassword;
+    EditText etRepeatPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etName.findViewById(R.id.etSignUpName);
+
+
     }
 }
