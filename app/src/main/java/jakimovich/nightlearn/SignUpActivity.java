033078a38c2f511ced39195215c2e0208988e567
BitterToGitter <jakimowichmaxim@gmail.com>
2024-02-20 09:01:16 +0200

    Nightlearncheck

--- app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
+++ app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
@@ -3,6 +3,7 @@ package jakimovich.nightlearn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -14,13 +15,20 @@ public class SignUpActivity extends AppCompatActivity {
     EditText etPassword;
     EditText etRepeatPassword;
 
+    Button BtnContinue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         etName.findViewById(R.id.etSignUpName);
-
+        etLastname.findViewById(R.id.etSignUpLastname);
+        etNickname.findViewById(R.id.etSignUpNickname);
+        etEmail.findViewById(R.id.etSignUpEmail);
+        etPassword.findViewById(R.id.etSignUpPassword);
+        etRepeatPassword.findViewById(R.id.etSignUpRepeatPassword);
+        BtnContinue.findViewById(R.id.signUpContinue);
 
     }
 }
