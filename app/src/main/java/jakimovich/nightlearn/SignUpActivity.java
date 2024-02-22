0ecf8449c4aa8c3dc25bc55453ad9f4a06590577
BitterToGitter <jakimowichmaxim@gmail.com>
2024-02-22 10:18:38 +0200

    Nightlearncheck

--- app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
+++ app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
@@ -15,20 +15,20 @@ public class SignUpActivity extends AppCompatActivity {
     EditText etPassword;
     EditText etRepeatPassword;
 
-    Button BtnContinue;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        etName.findViewById(R.id.etSignUpName);
-        etLastname.findViewById(R.id.etSignUpLastname);
-        etNickname.findViewById(R.id.etSignUpNickname);
-        etEmail.findViewById(R.id.etSignUpEmail);
-        etPassword.findViewById(R.id.etSignUpPassword);
-        etRepeatPassword.findViewById(R.id.etSignUpRepeatPassword);
-        BtnContinue.findViewById(R.id.signUpContinue);
+        etName = findViewById(R.id.etSignUpName);
+        etLastname = findViewById(R.id.etSignUpLastname);
+        etNickname = findViewById(R.id.etSignUpNickname);
+        etEmail = findViewById(R.id.etSignUpEmail);
+        etPassword = findViewById(R.id.etSignUpPassword);
+        etRepeatPassword = findViewById(R.id.etSignUpRepeatPassword);
+        //btnContinue = findViewById(R.id.signUpContinue);
 
     }
 }
