7d9c867f08e52235a78a6946b09fd235b3ffbbf6
BitterToGitter <jakimowichmaxim@gmail.com>
2024-02-27 20:52:37 +0200

    Nightlearncheck

--- app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
+++ app/src/main/java/jakimovich/nightlearn/SignUpActivity.java
@@ -3,19 +3,21 @@ package jakimovich.nightlearn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText etName;
-    EditText etLastname;
-    EditText etNickname;
-    EditText etEmail;
-    EditText etPassword;
-    EditText etRepeatPassword;
-
-    Button btnContinue;
+    EditText etName, etLastname, etNickname, etEmail, etPassword, etRepeatPassword;
+    TextView tvBntContinue;
+    LinearLayout btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,34 @@ public class SignUpActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.etSignUpEmail);
         etPassword = findViewById(R.id.etSignUpPassword);
         etRepeatPassword = findViewById(R.id.etSignUpRepeatPassword);
-        //btnContinue = findViewById(R.id.signUpContinue);
+        tvBntContinue = findViewById(R.id.signUpContinueText);
+        btnContinue = findViewById(R.id.signUpContinue);
+
+        btnContinue.setOnClickListener(v -> signUp());
 
     }
+
+    private void signUp(){
+        String name = etName.getText().toString();
+        String lastname = etLastname.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String repeatPassword = etRepeatPassword.getText().toString();
+
+        if(name.isEmpty() || lastname.isEmpty() || email.isEmpty() || password.isEmpty() || repeatPassword.isEmpty()){
+            Toast.makeText(this, "Please enter all the data", Toast.LENGTH_SHORT).show();
+
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(this, "User has been created succesfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Error:" + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
 }
